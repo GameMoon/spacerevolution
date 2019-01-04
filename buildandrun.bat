743d4f6a9e85a2a122ad26bb2280a4ecf659b996
David Mikaczo <nevermind5214@gmail.com>
2019-01-04 18:13:19 +0100

    new build script

--- buildandrun.bat
+++ buildandrun.bat
@@ -7,5 +7,8 @@ start /I /W build.bat emcc
 @echo off
 echo Run
 @echo on
-start index.html
+start python -m http.server 8080
+start python -m SimpleHTTPServer 8080
+start http://localhost:8080/index.html
+pause
 EXIT
