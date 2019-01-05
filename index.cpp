a1792c041f71a365a39d7c909cfe3c77dd3223a9
Daniel Ugrin <ugrin.daniel@webdesign.co.hu>
2019-01-05 12:40:29 +0100

    Main render looped added to c ( render optimizations )

--- index.cpp
+++ index.cpp
@@ -10,43 +10,10 @@ const int SIZE = WIDTH * HEIGHT * 4;
 
 uint8_t data[SIZE];
 
-int main(void)
-{
-     printf("Hello world C++\n");
-    //  {for(int k = 0; k <SIZE;k++){
-    //      data[k] = k;
-    //  }}
-}
-
-
 
 extern "C"
 {
-    EMSCRIPTEN_KEEPALIVE int myFunction(int test)
-    {
-        printf("MyFunction Called\n");
-        return test*test;
-    }
-
-    EMSCRIPTEN_KEEPALIVE void update(int value)
-    {
-        uint8_t color = 0;
-        int r = 0;
-        for (int i = 0; i < SIZE; i+=4)
-        {
-            r = rand() % 2;
-            
-            if(r == 0) color = 0;
-            else color = 255;
-
-            data[i] = color;
-            data[i + 1] = color;
-            data[i + 2] = color;
-            data[i+3] = 255;
-        }
-    }
-
-    EMSCRIPTEN_KEEPALIVE uint8_t *getMemoryOffset()
+    EMSCRIPTEN_KEEPALIVE uint8_t * getMemoryOffset()
     {
         return &data[0];
     }
@@ -59,4 +26,40 @@ extern "C"
     {
         return HEIGHT;
     }
+
+    EM_JS(void, render, (), {
+        Module.renderImageFromMemory();
+    });
+
+    void updateLoop()
+    {
+        uint8_t color = 0;
+        int r = 0;
+        for (int i = 0; i < SIZE; i += 4)
+        {
+            r = rand() % 2;
+
+            if (r == 0)
+                color = 0;
+            else
+                color = 255;
+
+            data[i] = color;
+            data[i + 1] = color;
+            data[i + 2] = color;
+            data[i + 3] = 255;
+        }
+
+        render();
+    }
+    
 }
+
+
+int main(void)
+{
+    emscripten_set_main_loop(updateLoop, 0, 1);
+}
+
+
+
