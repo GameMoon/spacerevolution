d1057d9b38773bf0404bf827b07f3dce50df557b
Daniel Ugrin <ugrin.daniel@webdesign.co.hu>
2019-01-05 14:07:32 +0100

    Rectangle movement, rotation demo added

--- index.cpp
+++ index.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <math.h>
 #include <emscripten/emscripten.h>
 
 const int WIDTH = 800;
@@ -30,31 +31,65 @@ extern "C"
     EM_JS(void, render, (), {
         Module.renderImageFromMemory();
     });
-
-    void updateLoop()
-    {
-        uint8_t color = 0;
-        int r = 0;
-        for (int i = 0; i < SIZE; i += 4)
-        {
-            r = rand() % 2;
-
-            if (r == 0)
-                color = 0;
-            else
-                color = 255;
-
-            data[i] = color;
-            data[i + 1] = color;
-            data[i + 2] = color;
-            data[i + 3] = 255;
-        }
-
-        render();
-    }
-    
+   
 }
 
+void setPixel(int x, int y, int r, int g, int b)
+{
+    int offset = (x + y * WIDTH) * 4;
+    data[offset] = r;
+    data[offset + 1] = g;
+    data[offset + 2] = b;
+    data[offset + 3] = 255;
+}
+
+void rotate(int x, int y,int centerX, int centerY, float omega)
+{
+    float translatedX = (x - centerX) * cos(omega) - (y - centerY) * sin(omega);
+    float translatedY = (x - centerX) * sin(omega) + (y - centerY) * cos(omega);
+
+    setPixel(translatedX+centerX, translatedY+centerY, 200, 0, 0);
+}
+
+void drawRectangle(int x, int y, int width, int height, float omega = 0.0f)
+{
+    int centerX = x+width/2;
+    int centerY = y + height /2;
+    for (int xOffset = 0; xOffset < width; xOffset++)
+    {
+        for (int yOffset = 0; yOffset < height; yOffset++)
+        {
+            rotate(x + xOffset, y + yOffset, centerX, centerY, omega);
+        }
+    }
+}
+
+void clearScreen(){
+    for (int i = 0; i < SIZE; i += 4)
+    {
+        data[i] = 255;
+        data[i + 1] = 255;
+        data[i + 2] = 255;
+        data[i + 3] = 255;
+    }
+}
+
+int startPos = 10;
+float omega = 0.78f;
+void updateLoop()
+{
+    
+    clearScreen();
+
+    drawRectangle(100, 100, 48, 48, 0.0);
+
+    drawRectangle(startPos, 50, 48, 48, omega);
+
+    omega += 0.1f;
+    startPos += 2;
+
+    render();
+}
 
 int main(void)
 {
