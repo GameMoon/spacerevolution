8c5059b0d273b7515d4b17c77227aae6d120c11f
David Mikaczo <nevermind5214@gmail.com>
2019-01-12 16:14:52 +0100

    Minor changes to Map Editor

--- MapeditorSpaceRevolution/MapeditorSpaceRevolution/Form_editor.Designer.cs
+++ MapeditorSpaceRevolution/MapeditorSpaceRevolution/Form_editor.Designer.cs
@@ -28,21 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.toolStrip_editwindow = new System.Windows.Forms.ToolStrip();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.toolStrip_File = new System.Windows.Forms.ToolStripDropDownButton();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.loadMapFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadTilesetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip_editwindow.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // toolStrip_editwindow
+            // 
+            this.toolStrip_editwindow.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStrip_File});
+            this.toolStrip_editwindow.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip_editwindow.Name = "toolStrip_editwindow";
+            this.toolStrip_editwindow.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip_editwindow.TabIndex = 0;
+            this.toolStrip_editwindow.Text = "toolStrip_editor";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 25);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.splitContainer1.Size = new System.Drawing.Size(800, 425);
+            this.splitContainer1.SplitterDistance = 143;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // toolStrip_File
+            // 
+            this.toolStrip_File.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.loadMapFileToolStripMenuItem,
+            this.loadTilesetToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.exitToolStripMenuItem});
+            this.toolStrip_File.Name = "toolStrip_File";
+            this.toolStrip_File.Size = new System.Drawing.Size(38, 22);
+            this.toolStrip_File.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save...";
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As...";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // loadMapFileToolStripMenuItem
+            // 
+            this.loadMapFileToolStripMenuItem.Name = "loadMapFileToolStripMenuItem";
+            this.loadMapFileToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.loadMapFileToolStripMenuItem.Text = "Load Map File...";
+            this.loadMapFileToolStripMenuItem.Click += new System.EventHandler(this.loadMapFileToolStripMenuItem_Click);
+            // 
+            // loadTilesetToolStripMenuItem
+            // 
+            this.loadTilesetToolStripMenuItem.Name = "loadTilesetToolStripMenuItem";
+            this.loadTilesetToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.loadTilesetToolStripMenuItem.Text = "Load Tileset...";
+            this.loadTilesetToolStripMenuItem.Click += new System.EventHandler(this.loadTilesetToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(177, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
             // Form_editor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.toolStrip_editwindow);
             this.Name = "Form_editor";
             this.Text = "Space Revolution - Map Editor";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form_editor_FormClosed);
+            this.toolStrip_editwindow.ResumeLayout(false);
+            this.toolStrip_editwindow.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip_editwindow;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStrip_File;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem loadMapFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadTilesetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
     }
 }
--- MapeditorSpaceRevolution/MapeditorSpaceRevolution/Form_neworload.cs
+++ MapeditorSpaceRevolution/MapeditorSpaceRevolution/Form_neworload.cs
@@ -7,14 +7,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO;
 
 namespace MapeditorSpaceRevolution
 {
     public partial class Form_neworload : Form
     {
-        public static string mapFilePath;
-        public static string tileFilePath;
 
         public Form_neworload()
         {
@@ -23,58 +20,23 @@ namespace MapeditorSpaceRevolution
 
         private void button_newmap_Click(object sender, EventArgs e)
         {
-            SaveFileDialog newMapFile = new SaveFileDialog();
-            newMapFile.Filter = "SR Map|*.srm";
-            newMapFile.Title = "Create new SR map...";
-            if (newMapFile.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-            {
-                mapFilePath = newMapFile.FileName;
-            }
-
-            OpenFileDialog openTileFile = new OpenFileDialog();
-            openTileFile.Filter = "All Files|*.*";
-            openTileFile.Title = "Select a Tileset File";
-
-            if (openTileFile.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-            {
-                tileFilePath = openTileFile.FileName;
-            }
-
+            loadsavepoopper.SavFileAs();
+            loadsavepoopper.LoadTileDialog();
             Form_editor editorWindow = new Form_editor();
             editorWindow.Show();
             this.Hide();
         }
+       
 
         private void button_loadmap_Click(object sender, EventArgs e)
         {
-            OpenFileDialog openMapFile = new OpenFileDialog();
-            openMapFile.Filter = "SR Map|*.srm|All Files|*.*";
-            openMapFile.Title = "Select a Map File";
-
-            if (openMapFile.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-            {
-                mapFilePath = openMapFile.FileName;
-            }
-
-            OpenFileDialog openTileFile = new OpenFileDialog();
-            openTileFile.Filter = "All Files|*.*";
-            openTileFile.Title = "Select a Tileset File";
-
-            if (openTileFile.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-            {
-                tileFilePath = openTileFile.FileName;
-            }
-
+            loadsavepoopper.LoaFile();
+            loadsavepoopper.LoadTileDialog();
             Form_editor editorWindow = new Form_editor();
             editorWindow.Show();
             this.Hide();
         }
 
-        public void progEnd()
-        {
-            Environment.Exit(0);
-        }
     }
-
 
 }
