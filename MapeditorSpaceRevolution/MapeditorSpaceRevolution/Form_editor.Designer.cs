3a0e0b1572dfaa5bf80e859a41f1c080c26eaead
David Mikaczo <nevermind5214@gmail.com>
2019-01-20 14:49:59 +0100

    Map editor tile slicing added

--- MapeditorSpaceRevolution/MapeditorSpaceRevolution/Form_editor.Designer.cs
+++ MapeditorSpaceRevolution/MapeditorSpaceRevolution/Form_editor.Designer.cs
@@ -38,12 +38,13 @@
             this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button_nextmap = new System.Windows.Forms.Button();
+            this.label_lnamedontchangethis = new System.Windows.Forms.Label();
+            this.button_prev = new System.Windows.Forms.Button();
+            this.label_levelname = new System.Windows.Forms.Label();
             this.toolStrip_editwindow.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
-            this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // toolStrip_editwindow
@@ -75,6 +76,7 @@
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
             this.saveToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
             this.saveToolStripMenuItem.Text = "Save...";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
             // 
             // saveAsToolStripMenuItem
             // 
@@ -127,27 +129,60 @@
             // 
             // splitContainer1.Panel2
             // 
-            this.splitContainer1.Panel2.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Panel2.BackColor = System.Drawing.Color.White;
             this.splitContainer1.Size = new System.Drawing.Size(800, 425);
             this.splitContainer1.SplitterDistance = 143;
             this.splitContainer1.TabIndex = 1;
             this.splitContainer1.SplitterMoved += new System.Windows.Forms.SplitterEventHandler(this.splitContainer1_SplitterMoved);
             this.splitContainer1.SizeChanged += new System.EventHandler(this.splitContainer1_SizeChanged);
             // 
-            // pictureBox1
+            // button_nextmap
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(3, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(33, 33);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.button_nextmap.Location = new System.Drawing.Point(725, 0);
+            this.button_nextmap.Name = "button_nextmap";
+            this.button_nextmap.Size = new System.Drawing.Size(75, 23);
+            this.button_nextmap.TabIndex = 2;
+            this.button_nextmap.Text = "Next Level >";
+            this.button_nextmap.UseVisualStyleBackColor = true;
+            this.button_nextmap.Click += new System.EventHandler(this.button_nextmap_Click);
+            // 
+            // label_lnamedontchangethis
+            // 
+            this.label_lnamedontchangethis.AutoSize = true;
+            this.label_lnamedontchangethis.Location = new System.Drawing.Point(403, 5);
+            this.label_lnamedontchangethis.Name = "label_lnamedontchangethis";
+            this.label_lnamedontchangethis.Size = new System.Drawing.Size(36, 13);
+            this.label_lnamedontchangethis.TabIndex = 3;
+            this.label_lnamedontchangethis.Text = "Level:";
+            // 
+            // button_prev
+            // 
+            this.button_prev.Location = new System.Drawing.Point(641, 0);
+            this.button_prev.Name = "button_prev";
+            this.button_prev.Size = new System.Drawing.Size(78, 23);
+            this.button_prev.TabIndex = 4;
+            this.button_prev.Text = "< Prev. Level";
+            this.button_prev.UseVisualStyleBackColor = true;
+            this.button_prev.Click += new System.EventHandler(this.button_prev_Click);
+            // 
+            // label_levelname
+            // 
+            this.label_levelname.AutoSize = true;
+            this.label_levelname.Location = new System.Drawing.Point(446, 6);
+            this.label_levelname.Name = "label_levelname";
+            this.label_levelname.Size = new System.Drawing.Size(35, 13);
+            this.label_levelname.TabIndex = 5;
+            this.label_levelname.Text = "label1";
             // 
             // Form_editor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label_levelname);
+            this.Controls.Add(this.button_prev);
+            this.Controls.Add(this.label_lnamedontchangethis);
+            this.Controls.Add(this.button_nextmap);
             this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.toolStrip_editwindow);
             this.Name = "Form_editor";
@@ -157,10 +192,8 @@
             this.Resize += new System.EventHandler(this.Form_editor_Resize);
             this.toolStrip_editwindow.ResumeLayout(false);
             this.toolStrip_editwindow.PerformLayout();
-            this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -178,6 +211,9 @@
         private System.Windows.Forms.ToolStripMenuItem loadTilesetToolStripMenuItem;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button_nextmap;
+        private System.Windows.Forms.Label label_lnamedontchangethis;
+        private System.Windows.Forms.Button button_prev;
+        private System.Windows.Forms.Label label_levelname;
     }
 }
--- MapeditorSpaceRevolution/MapeditorSpaceRevolution/Form_editor.cs
+++ MapeditorSpaceRevolution/MapeditorSpaceRevolution/Form_editor.cs
@@ -16,17 +16,18 @@ namespace MapeditorSpaceRevolution
     public partial class Form_editor : Form
     {
         public List<mapdata> terkepek = new List<mapdata>();
+        public int selectedlevel = 0;
+        public PictureBox[,] editarea = new PictureBox[32,24];
+        public List<Image> tileassets = new List<Image>();
+
+
 
         public Form_editor()
         {
             InitializeComponent();
+            initEditArea();
             seteditwinsize();
-
-
-            //dataGridView_mapdata.i
-            /*loadsavepoopper.mapFilePath;
-            loadsavepoopper.tileFilePath;*/
-            //loadMapData();
+            label_levelname.Text = "";
             drawimg();
         }
 
@@ -38,6 +39,7 @@ namespace MapeditorSpaceRevolution
         private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
         {
             loadsavepoopper.SavFileAs();
+            saveMapData();
         }
 
         private void loadMapFileToolStripMenuItem_Click(object sender, EventArgs e)
@@ -49,6 +51,7 @@ namespace MapeditorSpaceRevolution
         private void loadTilesetToolStripMenuItem_Click(object sender, EventArgs e)
         {
             loadsavepoopper.LoadTileDialog();
+            loadTileData();
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
@@ -92,8 +95,49 @@ namespace MapeditorSpaceRevolution
                     }
                 }
                 sr.Close();
+                label_levelname.Text = (selectedlevel + 1) + ". Level: " + terkepek[selectedlevel].name;
+                drawimg();
             }
 
+        }
+
+        private void saveMapData()
+        {
+            string path = loadsavepoopper.mapFilePath;
+            if (File.Exists(path)) File.Delete(path);
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                for (int i = 0; i < terkepek.Count(); i++)
+                {
+                    sw.WriteLine(terkepek[i].name);
+                    for (int j = 0; j < 24; j++)
+                    {
+                        for (int k = 0; k < 32; k++)
+                        {
+                            sw.Write(terkepek[i].tiledata[j, k]);
+                            if (k != 31) sw.Write(";");
+                            if(k==31) sw.WriteLine();
+                        }
+                    }
+                    for (int l = 0; l < terkepek[i].entities.Count(); l++)
+                    {
+                        sw.WriteLine(terkepek[i].entities[l].entid + ";" + terkepek[i].entities[l].xcoord + ";" + terkepek[i].entities[l].ycoord);
+                    }
+                    sw.Write("--");
+                }
+                sw.Close();
+            }
+        }
+
+        private void loadTileData()
+        {
+            Bitmap tilefile = new Bitmap(loadsavepoopper.tileFilePath);
+            for (int i = 0; i < 24; i++)
+            {
+                Rectangle cutframe = new Rectangle(0, 0, 32, 32);
+                tileassets.Add((Bitmap)tilefile.Clone(cutframe, tilefile.PixelFormat));
+            }
+            drawimg();
         }
 
         private void splitContainer1_Panel1_SizeChanged(object sender, EventArgs e)
@@ -123,10 +167,44 @@ namespace MapeditorSpaceRevolution
             dataGridView_tiles.Width = splitContainer1.Panel1.Width - 9;
             dataGridView_tiles.Height = splitContainer1.Panel1.Height - 9;*/
         }
+        private void initEditArea()
+        {
+            for (int i = 0; i < 24; i++)
+            {
+                for (int j = 0; j < 32; j++)
+                {
+                    editarea[j, i] = new PictureBox();
+                    editarea[j, i].Parent = splitContainer1.Panel2;
+                    editarea[j, i].Width = 32;
+                    editarea[j, i].Height = 32;
+                    editarea[j, i].Location = new Point((j * 32) + j + 1, (i * 32) + i + 1);
+                    editarea[j, i].BackColor = Color.Black;
+                    editarea[j, i].Click += new EventHandler(editTileClick);
+                }
+            }
+        }
+
         private void drawimg()
         {
-            //System.Drawing.Image.FromFile(loadsavepoopper.tileFilePath);
+            for (int i = 0; i < 24; i++)
+            {
+                for (int j = 0; j < 32; j++)
+                {
+                    if (tileassets.Count!=0 && terkepek.Count!=0)
+                    {
+                        editarea[j, i].Image = (Image)tileassets[terkepek[selectedlevel].tiledata[i, j]];
+                    }
+                }
+            }
         }
+
+        private void editTileClick(object sender, EventArgs e)
+        {
+            PictureBox item = (PictureBox)sender;
+            item.BackColor = Color.Aqua;
+            drawimg();
+        }
+
         public class entitydata
         {
             public int entid;
@@ -139,6 +217,32 @@ namespace MapeditorSpaceRevolution
             public string name;
             public int[,] tiledata = new int[24, 32];
             public List<entitydata> entities = new List<entitydata>();
+        }
+        
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveMapData();
+        }
+
+        private void button_prev_Click(object sender, EventArgs e)
+        {
+            if (terkepek.Count != 0)
+            {
+                if (selectedlevel > 0) selectedlevel--;
+                label_levelname.Text = (selectedlevel + 1) + ". Level: " + terkepek[selectedlevel].name;
+            }
+            drawimg();
+        }
+
+        private void button_nextmap_Click(object sender, EventArgs e)
+        {
+            if (terkepek.Count != 0)
+            {
+                if (selectedlevel < terkepek.Count()-1) selectedlevel++;
+                label_levelname.Text = (selectedlevel + 1) + ". Level: " + terkepek[selectedlevel].name;
+            }
+            drawimg();
         }
     }
 }
